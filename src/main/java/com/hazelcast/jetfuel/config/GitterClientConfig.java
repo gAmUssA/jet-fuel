4f0d1d5baf1dbdb646c2e6332d5624041ed30e57
Viktor Gamov <gamussa@gmail.com>
2017-03-15 14:45:39 -0400

    added OkHttpClient to prevent drops of connection

--- src/main/java/com/hazelcast/jetfuel/config/GitterClientConfig.java
+++ src/main/java/com/hazelcast/jetfuel/config/GitterClientConfig.java
@@ -4,11 +4,15 @@ import com.amatkivskiy.gitter.sdk.model.response.message.MessageResponse;
 import com.amatkivskiy.gitter.sdk.rx.client.RxGitterStreamingApiClient;
 import com.google.gson.Gson;
 import com.hazelcast.core.IMap;
+import com.squareup.okhttp.OkHttpClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import retrofit.client.OkClient;
 import rx.functions.Action1;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @Slf4j
@@ -18,10 +22,21 @@ public class GitterClientConfig {
     String roomId;
 
     @Bean
+    OkHttpClient getOkHttpClient() {
+        OkHttpClient okClient = new OkHttpClient();
+
+        // Configure OkHttpClient not to drop connection rapidly.
+        okClient.setReadTimeout(10, TimeUnit.MINUTES);
+        return okClient;
+    }
+
+    @Bean
     RxGitterStreamingApiClient gitterStreamingApiClient(@Value("${gitter.token}") String token,
-                                                        Action1<MessageResponse> messageResponseAction1) {
+                                                        Action1<MessageResponse> messageResponseAction1,
+                                                        OkHttpClient httpClient) {
         final RxGitterStreamingApiClient gitterClient = new RxGitterStreamingApiClient.Builder()
                 .withAccountToken(token)
+                .withClient(new OkClient(httpClient))
                 .build();
 
         gitterClient.getRoomMessagesStream(roomId).subscribe(messageResponseAction1);
